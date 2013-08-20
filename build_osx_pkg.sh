dec47587c1d3ce90f8156b2b13b2fe4299246e1f
Evan Liu <hmisty@gmail.com>
2013-08-20 15:50:51 +0800

    clean

--- build_osx_pkg.sh
+++ build_osx_pkg.sh
@@ -2,10 +2,17 @@
 
 # build osx pkg
 # install location: /Library/quicklojure
-mkdir -p .tmp/usr/bin .tmp/Library
-cp -R quicklojure .tmp/Library/
+# package structure
+# /usr/bin/clj
+# /Library
+#   + quicklojure/*
+#   + lib/*.jar
+#
+mkdir -p .tmp/usr/bin .tmp/Library/quicklojure
+cp -R src/* .tmp/Library/quicklojure/
+cp -R lib .tmp/Library/quicklojure/
 cd .tmp/usr/bin
 ln -s /Library/quicklojure/clj clj
 cd -
 pkgbuild --identifier quicklojure.pkg.app --root .tmp quicklojure.pkg
-#rm -rf .tmp
+rm -rf .tmp
