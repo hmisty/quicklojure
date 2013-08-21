af6b8171a58078408209a9528515c2e64ece8092
Evan Liu <hmisty@gmail.com>
2013-08-21 23:26:51 +0800

    add current location into classpath

--- src/gen_completions.clj
+++ src/gen_completions.clj
@@ -1,7 +1,8 @@
-(def completions  mapcat  comp keys ns-publics)  all-ns)))
+(def completions (mapcat (comp keys ns-publics) (all-ns)))
 
 (with-open  [f  (java.io.BufferedWriter.  (java.io.FileWriter.  (str
     (System/getenv "HOME") "/.clj_completions")))]   
 (.write f  (apply str
     (interpose \newline completions))))
 
+(defn add [a b] (+ a b))
